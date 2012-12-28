228b7dec397708b0f27c98f83246c0e614d032ee
Bram Gotink <bram.gotink@vtk.be>
2012-12-28 19:43:55 +0100

    country framework in place

--- src/model/country.js
+++ src/model/country.js
@@ -1,13 +1,46 @@
-exports.Country = function() {
-	print("Country!");
-    return {
-        City: function() {
-            print("City!");
-            return {
-                Airport: function () {
-                    print("Airport!");
-                }
-            }
-        }
-    };
+var AirportCreator = function(airport, city) {
+    this.airport = airport;
+    this.city = city;
+    print("Airport " + airport.name + " created.");
+};
+
+AirportCreator.prototype.Airport = function(args) {
+    return this.city.Airport(args);
+}
+
+AirportCreator.prototype.City = function(args) {
+    return this.city.City(args);
+}
+
+var CityCreator = function(city, country) {
+    this.city = city;
+    this.country = country;
+    this.airports = [];
+    print("City " + city.name + " created");
+}
+
+CityCreator.prototype.Airport = function(args) {
+    var airport = new AirportCreator(args, this)
+    this.airports.push(airport);
+    return airport;
+}
+
+CityCreator.prototype.City = function(args) {
+    return this.country.City(args);
+}
+
+var CountryCreator = function(country) {
+    this.country = country;
+    this.cities = [];
+    print("Country " + country.name + " created");
+}
+
+CountryCreator.prototype.City = function(args) {
+    var city = new CityCreator(args, this)
+    this.cities.push(city);
+    return city;
+}
+
+exports.Country = function(args) {
+	return new CountryCreator(args);
 };
