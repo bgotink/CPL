66fcafb4ba6bd3a89f07f91a7900e335977ae3b8
Geert <geertvc@gmail.com>
2012-12-29 11:28:26 +0100

    aircrafttype implemented like country was, also option to create seat when created aircrafttype

--- src/model/aircrafttype.js
+++ src/model/aircrafttype.js
@@ -1,3 +1,24 @@
-exports.AircraftType = function() {
-	print("AircraftType!");
+var SeatCreator = function(seat, aircrafttype) {
+    this.aircrafttype = aircrafttype;
+    print("Seat with row: " + seat.row + " and letter: " + seat.letter + " created");
+}
+
+SeatCreator.prototype.Seat = function(args) {
+    return this.aircrafttype.Seat(args);
+}
+
+var AircraftTypeCreator = function(aircrafttype) {
+    this.aircrafttype = aircrafttype;
+    this.seats = [];
+    print("AircraftType with model: " + aircrafttype.model + " and manufacturer: " + aircrafttype.manufacturer + " created");
+}
+
+AircraftTypeCreator.prototype.Seat = function(args) {
+    var seat = new SeatCreator(args, this)
+    this.seats.push(seat);
+    return seat;
+}
+
+exports.AircraftType = function(args) {
+	return new AircraftTypeCreator(args);
 };
