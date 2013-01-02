505d8759ff94481bb66e7f3de40152685b738177
Bram Gotink <bram.gotink@vtk.be>
2013-01-02 16:49:11 +0100

    apparently, [].map is already present perhaps I've made a mistake when testing for its existence before

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,4 @@
 /*
- * [].map(Function)
- */
-
-Object.defineProperty(
-    Array.prototype,
-    'map',
-    {
-        enumerable: false,
-        configurable: false,
-        writable: false,
-        value: function (func) {
-            var dest = [];
-            this.forEach(
-                function(e) {
-                    dest.push(func(e));
-                }
-            );
-            return dest;
-        }
-    }
-)
-
-module.exports.map = function(src, func) {
-    return src.map(func);
-}
-
-/*
  * MultiIndexedSet
  */
  
