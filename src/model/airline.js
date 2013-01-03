21b29a39a6e2c0ee63336ca9ee133911681afd8e
Bram Gotink <bram.gotink@vtk.be>
2013-01-03 14:07:14 +0100

    added get

--- src/model/airline.js
+++ src/model/airline.js
@@ -1,17 +1,29 @@
 var Sequelize   = require('sequelize')
-  , db          = require('../db');
+  , db          = require('../db')
+  , Utils       = require('../utils');
 
 var AirlineCreator = function(airline) {
 	if(!airline.name) throw "name attribute of airline missing";
 	if(!airline.code) throw "code attribute of airline missing";
 
     this.airline = db.Airline.build(airline, ['name', 'code']);
+    airlines.push(this);
     
     db.applyLater(this.airline, 'save');
     
     print("Airline with name: " + airline.name + " and code: " + airline.code + " created");
 }
 
+AirlineCreator.prototype.getDO = function () {
+    return this.airline;
+}
+
+var airlines = new Utils.MultiIndexedSet(['code']);
+
 exports.Airline = function(args) {
 	return new AirlineCreator(args);
 };
+
+exports.Airline.get = function(args) {
+    return airlines.get(args);
+}
